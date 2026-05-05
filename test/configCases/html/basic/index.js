5b67313dc7145f9596fd6c845b044f9ef5866416
aryanraj45 <rajaryan445567@gmail.com>
2026-05-05 19:08:38 +0530

    test: add basic html config test case and remove gsoc doc

---
+++ test/configCases/html/basic/index.js
@@ -0,0 +1,7 @@
+import page from "./page.html";
+
+it("should compile and export html as string", () => {
+	expect(typeof page).toBe("string");
+	expect(page).toContain("<h1>Hello World</h1>");
+	expect(page).toContain("<!DOCTYPE html>");
+});
